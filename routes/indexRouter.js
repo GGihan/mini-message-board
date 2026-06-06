45f33dbd83abe87024dae9e4b0f57e7629ce86e8
ggihan <140435642+GGihan@users.noreply.github.com>
2026-06-06 11:46:20 +0200

    Add example messages and display each of them

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,9 +1,31 @@
-import { Router } from "express";
+import { Router, text } from "express";
 
 const indexRouter = Router();
 
+const messages = [
+  {
+    text: 'Hey, what is going on?',
+    user: 'Jeff',
+    added: new Date(),
+  },
+  {
+    text: 'Hello, World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+  {
+    text: 'Welcome to the Club!',
+    user: 'Chris',
+    added: new Date(),
+  },
+];
+
 indexRouter.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
+});
+
+indexRouter.get('/new', (req, res) => {
+
 });
 
 export default indexRouter;
